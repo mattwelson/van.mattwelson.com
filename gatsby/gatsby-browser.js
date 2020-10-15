b0c0af8bb83b9026b4b1b0211e79025cf3aaba90
mattwelson <matthew.welson@gmail.com>
2020-10-16 09:18:55 +1300

    feat added smooth scroll handling

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -5,3 +5,33 @@
  */
 
 // You can delete this file if you're not using it
+
+function anchorScroll(location) {
+  // Check for location so build does not fail
+  if (location && location.hash) {
+    setTimeout(() => {
+      // document.querySelector(`${location.hash}`).scrollIntoView({ behavior: 'smooth', block: 'start' });
+      const item = document.querySelector(`${location.hash}`)?.offsetTop
+      if (!item) return
+
+      const mainNavHeight = document.querySelector(`nav`)?.offsetHeight || 50
+      window.scrollTo({
+        top: item - mainNavHeight,
+        left: 0,
+        behavior: 'smooth',
+      })
+    }, 0)
+  }
+}
+
+export const onRouteUpdate = ({ location }) => {
+  anchorScroll(location)
+  return true
+}
+export const shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition,
+}) => {
+  anchorScroll(location)
+  return true
+}
